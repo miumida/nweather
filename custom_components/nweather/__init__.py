50ed2408a16d5c5898fce235efcd85755d5abbe3
miumida <46196137+miumida@users.noreply.github.com>
2023-12-27 23:57:15 +0900

    Update __init__.py

--- custom_components/nweather/__init__.py
+++ custom_components/nweather/__init__.py
@@ -18,9 +18,10 @@ from homeassistant.const import (
     Platform,
 )
 
-from .const import DOMAIN, CONF_RGN_CD, BSE_URL, VAR_SUMMARY, ATTR_NOWFCAST, ATTR_AIRFCAST, ATTR_SUNRISE, ATTR_SUNSET, ATTR_UV
+from .const import DOMAIN, CONF_RGN_CD, BSE_URL, VAR_SUMMARY, VAR_API, VAR_HOURLY, ATTR_NOWFCAST, ATTR_HDAYFCAST, ATTR_AIRFCAST, ATTR_SUNRISE, ATTR_SUNSET, ATTR_NOW
+from .const import ATTR_UV, ATTR_GRADE, ATTR_LBTXT, ATTR_GTXTS, ATTR_GTXTD
 
-PLATFORMS = [Platform.WEATHER]
+PLATFORMS = [Platform.WEATHER, Platform.SENSOR]
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -73,7 +74,7 @@ async def async_update_entry(hass: HomeAssistant, config_entry: ConfigEntry):
 
 
 class NaverWeatherData:
-    """Keep data for Met Éireann weather entities."""
+    """Keep data for nweather entities."""
 
     def __init__(self, hass, config):
         """Initialise the weather entity data."""
@@ -90,26 +91,91 @@ class NaverWeatherData:
         self.airFcast = {}
         
         self.daily_forecast = None
-        self.hourly_forecast = None
+        self.hourly_forecast = []
+
+        self._newsCards = []
 
         self.sunrise = None
         self.sunset  = None
+
+
+    def get_attr(self, id):
+        attr = {}
+
+        if id == "uv":
+            attr = { ATTR_LBTXT  : self.uv[ATTR_LBTXT]
+                    , ATTR_GRADE : self.uv[ATTR_GRADE]
+                    , ATTR_GTXTS : self.uv[ATTR_GTXTS]
+                }
+        else:
+            attr = {}
+
+        return attr
+
+    def get_state(self, id):
+        state = ""
+
+        if id == "uv":
+            state = self.uv[ATTR_LBTXT]
+        elif id == "wetrTxt" or id == "tmpr" or id == "humd" or id == "stmpr" or id == "ytmpr" or id == "oneHourRainAmt" :
+            state = self.nowFcast_data[id]
+        elif id == "stationPM10" or id == "stationPM25" or id == "stationPM10Legend1" or id =="stationPM25Legend1" or id == "stationKhaiLegend1" or id == "stationName":
+            state = self.airFcast[id]
+        elif id == "news1" :
+            state = self._newsCards[0]["title"]
+        else:
+            state = ""
+
+        return state
+
+    def get_name(self, id):
+        name = ""
+
+        if id == "news1" :
+            name = self._newsCards[0]["officeHname"]
+        else:
+            name = ATTR_NOW[id][0]
+
+        return name
+        
 
     async def fetch_data(self):
         """Fetch data from API - (current weather and forecast)."""
         html = await self.getNweather(async_get_clientsession(self.hass), self._config[CONF_RGN_CD])
 
-        weatherSummary = self.str2Json(VAR_SUMMARY, html)
+        blockApiResult = self.str2Json(VAR_API, html)        
+
+        #weatherSummary = self.str2Json(VAR_SUMMARY, html)
+        choiceResult = blockApiResult["results"]["choiceResult"]
+        weatherSummary = choiceResult["nowSynthesisFcast~~1"]
+        
+        domesticWeeklyFcastList = choiceResult["weeklyFcast~~1"]["domesticWeeklyFcastList"]
+
+        _LOGGER.error(weatherSummary);
 
         self.nowFcast_data = weatherSummary[ATTR_NOWFCAST]
-        self.hdayFcastList = weatherSummary["hdayFcastList"]
+        self.hdayFcastList = domesticWeeklyFcastList
 
-        self.uv = weatherSummary[ATTR_UV]
+        lifeIndexData = choiceResult["lifeIndex~~1"]
+
+        for item in lifeIndexData:
+            clickCode = item["lifeIndexData"]["clickCode"]
+
+            if (clickCode == "uvindx"):
+                self.uv = item["lifeIndexData"]
 
         self.airFcast = weatherSummary[ATTR_AIRFCAST]
 
-        self.sunrise = weatherSummary[ATTR_SUNRISE]
-        self.sunset  = weatherSummary[ATTR_SUNSET]
+        sunRiseSet = choiceResult["sunRiseSet~~1"]["sunRiseSetList"][0]
+
+        self.sunrise = sunRiseSet[ATTR_SUNRISE]
+        self.sunset  = sunRiseSet[ATTR_SUNSET]
+
+        self.hourly_forecast = choiceResult["hourlyFcast~~1"]["domesticHourlyFcastList"]
+
+        self._newsCards = choiceResult["newsTodayList~~1"]["newsCards"]["News"]
+
+        #_LOGGER.error(self.nowFcast_data["stmpr"])
 
         return self
 
@@ -129,9 +195,9 @@ class NaverWeatherData:
             url = BSE_URL.format(rgnCd)
 
             res = await session.get(url, headers=hdr, timeout=60)
-
+        
             html = await res.text()
-
+            #_LOGGER.error(html)
             return html
         except Exception as ex:
             _LOGGER.error(f'[{DOMAIN}] Failed to update nweather API status Error: %s', ex)
@@ -140,13 +206,28 @@ class NaverWeatherData:
     def str2Json(self, reg, html):
         try:
             matches = re.findall(reg, html)
-                    
+            #_LOGGER.error(matches)
             varStr = re.findall(r"{[^;]+}", matches[0])[0]
 
             rslt = json.loads(varStr)
-            #_LOGGER.error(rslt)
+            #_LOGGER.error(varStr)
 
             return rslt
         except Exception as ex:
             _LOGGER.error(f'[{DOMAIN}] Failed to update nweather str2Json Error: %s', ex)
             return {}
+
+    def arr2Json(self, reg, html):
+        rslt = []
+        try:
+            matches = re.findall(reg, html)
+                    
+            varArr = re.findall(r"\[[^;]+\]", matches[0])[0]
+            
+            for item in json.loads(varArr):
+                rslt.append( item )
+
+            return rslt
+        except Exception as ex:
+            _LOGGER.error(f'[{DOMAIN}] Failed to update nweather arr2Json Error: %s', ex)
+            return []
